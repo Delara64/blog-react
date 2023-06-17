048766af59a0e2ff1a40e6bbb638c07e91299701
Delara64 <119980909+Delara64@users.noreply.github.com>
2023-06-16 18:23:23 -0700

    Update ArticlesList.js

--- components/ArticlesList.js
+++ components/ArticlesList.js
@@ -74,7 +74,7 @@ const ArticlesList = () => {
           </h2>
           {article.coverImage && (
             <div className={styles['article-cover-image-container']}>
-              <image
+              <img
                 src={article.coverImage}
                 alt={article.title}
                 className={styles['article-cover-image']}
